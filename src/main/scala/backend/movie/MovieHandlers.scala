b2cdcba34c4f55c20c97a017aa5cd614302ee1fa
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-05-24 14:27:21 +0500

    feat: add movie handlers

---
+++ src/main/scala/backend/movie/MovieHandlers.scala
@@ -0,0 +1,30 @@
+package backend.movie
+
+import zio.*
+
+import backend.data.dataSource
+import backend.data.repositories.MovieRepo
+import domain.*
+
+object MovieHandlers:
+  def addMovieHandler(movie: Movie): ZIO[MovieRepo, Error, Unit] =
+    ZIO.serviceWithZIO[MovieRepo](_.add(movie)).mapError(e => MovieError(e.getMessage))
+  
+  def getMovieByIdHandler(id: ID): ZIO[MovieRepo, Error, Movie] = 
+    ZIO.serviceWithZIO[MovieRepo](_.getById(id))
+      .foldZIO(
+        err => ZIO.fail(MovieError("Something went wrong")),
+        {
+          case Some(x) => ZIO.succeed(x)
+          case None => ZIO.fail(NoMovieWithGivenIdError())
+        }
+      )
+  
+  def getAllMoviesHandler: ZIO[MovieRepo, Error, Vector[Movie]] =
+    ZIO.serviceWithZIO[MovieRepo](_.getAll).mapError(e => MovieError(e.getMessage))
+    
+  def updateMovieHandler(id: ID, movie: Movie): ZIO[MovieRepo, Error, Unit] =
+    ZIO.serviceWithZIO[MovieRepo](_.updateMovie(id, movie)).mapError(e => MovieError(e.getMessage))
+
+  def removeMovieByIdHandler(id: ID): ZIO[MovieRepo, Error, Unit] =
+    ZIO.serviceWithZIO[MovieRepo](_.removeById(id)).mapError(e => MovieError(e.getMessage))
