f65ecb8f42eb9cc1314b1471e489237c4c0ec396
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-05-30 02:35:19 +0300

    feat: add gives for circe iron

--- src/main/scala/app/domain/Movie.scala
+++ src/main/scala/app/domain/Movie.scala
@@ -2,9 +2,16 @@ package app.domain
 
 import io.circe.*
 import io.circe.generic.semiauto.*
-import io.github.iltotore.iron.circe.given
+import io.github.iltotore.iron.circe.*
+
+import sttp.tapir.codec.iron.given
+import sttp.tapir.Schema
 
 import app.domain.MovieId
+
+given Encoder[MovieId] = Encoder[Long].contramap(identity)
+given Decoder[MovieId] = Decoder[Long].emap(MovieId.either)
+given Schema[MovieId] = Schema.schemaForLong.as[MovieId]
 
 case class Movie(
                   uid: MovieId, // ID of a user which the movie is associated with
