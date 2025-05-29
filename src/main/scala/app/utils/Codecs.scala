e0e6b97fa0eb2a24b39e0e04974f6df4b6d46c8b
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-05-30 02:34:41 +0300

    chore: add tapir codec

---
+++ src/main/scala/app/utils/Codecs.scala
@@ -0,0 +1,24 @@
+package app.utils
+
+import app.domain.{MovieId, MovieIdDescription, BaseIdType}
+import com.augustnagro.magnum.DbCodec
+import io.github.iltotore.iron.*
+import sttp.tapir.*
+import io.github.iltotore.iron.*
+import sttp.tapir.CodecFormat.TextPlain
+
+inline given ironDbCodec[T, Description](
+  using DbCodec[T], Constraint[T, Description]
+): DbCodec[T :| Description] =
+  DbCodec[T].biMap(
+    _.refineUnsafe[Description],
+    identity
+  )
+
+given Codec[String, MovieId, TextPlain] =
+  Codec.long.mapEither { long =>
+    long.refineEither[MovieIdDescription] match {
+      case Right(movieId: MovieId) => Right(movieId)
+      case Left(error) => Left(s"Invalid MovieId: $error")
+    }
+  }(_.assume[BaseIdType])
