6fced53366ef4ed6080f7ad9cece564b1002c746
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-05-30 02:35:37 +0300

    feat: add companion object to refiend types

--- src/main/scala/app/domain/TypeAliases.scala
+++ src/main/scala/app/domain/TypeAliases.scala
@@ -2,9 +2,14 @@ package app.domain
 
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.*
+import sttp.tapir.codec.iron
+
+type BaseIdType = Long
 
 type MovieIdDescription = DescribedAs[Greater[0], "Id must be greater than 0"]
-type MovieId = Long :| MovieIdDescription
+type MovieId = BaseIdType :| MovieIdDescription
+object MovieId extends RefinedType[BaseIdType, MovieIdDescription]
 
 type UserIdDescription = DescribedAs[Greater[0], "Id must be greater than 0"]
-type UserId = Long :| UserIdDescription
+type UserId = BaseIdType :| UserIdDescription
+object UserId extends RefinedType[BaseIdType, UserIdDescription]
