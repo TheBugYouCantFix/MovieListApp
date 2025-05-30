40f80b641f19cb68ee697a3fa144bd2a87f82078
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-05-30 21:26:16 +0300

    feat: add username refined type

--- src/main/scala/app/domain/TypeAliases.scala
+++ src/main/scala/app/domain/TypeAliases.scala
@@ -16,3 +16,10 @@ object UserId extends RefinedType[BaseIdType, UserIdDescription]
 type PasswordDescription = DescribedAs[MinLength[5], "Min password length is 5"]
 type Password = String :| PasswordDescription
 object Password extends RefinedType[String, PasswordDescription]
+
+type UsernameDescription = DescribedAs[
+  MinLength[2] & MaxLength[20] & Match["^[a-zA-Z0-9_-]{2,20}$"],
+  "2 <= length <= 20, + a default username regex"
+]
+type Username = String :| UsernameDescription
+object Username extends RefinedType[String, UsernameDescription]
