4b9cba51b8102737e1465cbf28794fd41ca2f33b
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-01 20:22:45 +0300

    feat: add tapir codecs

--- src/main/scala/app/utils/Codecs.scala
+++ src/main/scala/app/utils/Codecs.scala
@@ -3,7 +3,7 @@ package app.utils
 import app.domain.{MovieId, MovieIdDescription, BaseIdType}
 import com.augustnagro.magnum.DbCodec
 
-import sttp.tapir.Schema
+import sttp.tapir.{Schema, Codec, CodecFormat}
 
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.*
@@ -17,11 +17,22 @@ inline given ironDbCodec[T, Description](
     identity
   )
 
+
+// circe decoder codec
 given [A, C](using baseDecoder: Decoder[A], constraint: RuntimeConstraint[A, C]): Decoder[A :| C] =
   baseDecoder.emap(_.refineEither[C])
 
+// circe encoder codec
 given [A, C](using baseEncoder: Encoder[A]): Encoder[A :| C] =
   baseEncoder.contramap(identity)
 
-given [A, C](using baseSchema: Schema[A]): Schema[A :| C] =
-  baseSchema.map(Some(_))()
+// tapir schema codec
+given [A, C](using baseSchema: Schema[A], constraint: RuntimeConstraint[A, C]): Schema[A :| C] =
+  baseSchema.map(Some(_))(_.refineUnsafe[C]).as[A :| C]
+
+// tapir codec for types in paths
+given [A, C](using
+             baseCodec: Codec[String, A, CodecFormat.TextPlain],
+             constraint: RuntimeConstraint[A, C]
+            ): Codec[String, A :| C, CodecFormat.TextPlain] =
+baseCodec.mapEither(_.refineEither[C])(identity)
