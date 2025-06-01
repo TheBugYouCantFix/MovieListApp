42dba7e1d63683ed8bae18c2f5c3b73ac6ec2d33
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-01 20:05:34 +0300

    feat: create generic circe codecs

--- src/main/scala/app/utils/Codecs.scala
+++ src/main/scala/app/utils/Codecs.scala
@@ -3,9 +3,11 @@ package app.utils
 import app.domain.{MovieId, MovieIdDescription, BaseIdType}
 import com.augustnagro.magnum.DbCodec
 
-import sttp.tapir.*
+import sttp.tapir.Schema
+
 import io.github.iltotore.iron.*
-import sttp.tapir.CodecFormat.TextPlain
+import io.github.iltotore.iron.constraint.all.*
+import io.circe.*
 
 inline given ironDbCodec[T, Description](
   using DbCodec[T], Constraint[T, Description]
@@ -22,3 +24,8 @@ given Codec[String, MovieId, TextPlain] =
       case Left(error) => Left(s"Invalid MovieId: $error")
     }
   }(_.assume[BaseIdType])
+given [A, C](using baseDecoder: Decoder[A], constraint: RuntimeConstraint[A, C]): Decoder[A :| C] =
+  baseDecoder.emap(_.refineEither[C])
+
+given [A, C](using baseEncoder: Encoder[A]): Encoder[A :| C] =
+  baseEncoder.contramap(identity)
