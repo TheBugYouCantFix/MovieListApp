be0f4a5f72f007c84a65f8c282ce23373eebb555
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-01 20:23:50 +0300

    chore: change movie imports

--- src/main/scala/app/domain/Movie.scala
+++ src/main/scala/app/domain/Movie.scala
@@ -2,13 +2,8 @@ package app.domain
 
 import io.circe.*
 
-import sttp.tapir.Schema
-
-import app.domain.{MovieId, BaseIdType}
-
-given Encoder[MovieId] = Encoder[BaseIdType].contramap(identity)
-given Decoder[MovieId] = Decoder[BaseIdType].emap(MovieId.either)
-given Schema[MovieId] = Schema.schemaForLong.as[MovieId]
+import app.domain.MovieId
+import app.utils.given 
 
 case class Movie(
                   uid: MovieId, // ID of a user which the movie is associated with
