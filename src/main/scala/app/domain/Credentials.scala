c96a7b2988c061e1561581cff1678cb951e50613
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-02 13:26:32 +0300

    refactor: format Credentials

--- src/main/scala/app/domain/Credentials.scala
+++ src/main/scala/app/domain/Credentials.scala
@@ -1,12 +1,11 @@
 package app.domain
 
 import io.circe.*
-import io.circe.generic.auto.*
-import sttp.tapir.Schema
 
 import app.domain.Username
 import app.utils.given
 
-case class Credentials(username: Username, passwordHash: String) derives Codec.AsObject
-
-
+case class Credentials(
+                        username: Username,
+                        password: Password
+                      ) derives Codec.AsObject
