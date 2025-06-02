d0f01c006303748624d54b966dfb18bdb005e716
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-02 13:34:12 +0300

    refactor: remove unnecessary import

--- src/main/scala/app/backend/auth/request_models/UpdatePassword.scala
+++ src/main/scala/app/backend/auth/request_models/UpdatePassword.scala
@@ -2,7 +2,7 @@ package app.backend.auth.request_models
 
 import io.circe.*
 
-import app.domain.{Credentials, Username, Password}
+import app.domain.{Credentials, Password}
 import app.utils.given
 
 case class UpdatePassword(
