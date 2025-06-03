50c5fcf295ebf1136af084d0673db650fea79cd4
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-04 00:24:16 +0300

    feat: add auth errors

--- src/main/scala/app/domain/Error.scala
+++ src/main/scala/app/domain/Error.scala
@@ -3,6 +3,11 @@ package app.domain
 import io.circe.*
 
 sealed trait Error(message: String) derives Codec.AsObject
+
 case class MovieError(message: String) extends Error(message)
 case class NoMovieWithGivenIdError(message: String = "No movie with given id found") extends Error(message)
 
+
+case class AuthError(message: String) extends Error(message)
+case class PasswordHashingFailedError(message: String) extends Error(message)
+case class InvalidCredentialsError(message: String = "Invalid credentials") extends Error(message)
