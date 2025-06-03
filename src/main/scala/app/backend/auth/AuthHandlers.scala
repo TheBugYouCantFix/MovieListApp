6bb181b79d710a3f8ab8d525c16fc9ac5df2b30b
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-04 01:42:57 +0300

    refactor: order imports

--- src/main/scala/app/backend/auth/AuthHandlers.scala
+++ src/main/scala/app/backend/auth/AuthHandlers.scala
@@ -1,14 +1,11 @@
 package app.backend
 
-import app.backend.auth.jwt.JwtService
 import zio.*
 import com.github.roundrop.bcrypt.bcrypt
 
-import scala.util.{Failure, Success, Try}
 import app.backend.data.repositories.UserRepo
-import app.domain
-import app.domain.Credentials
-import zio.*
+import app.backend.auth.jwt.JwtService
+import app.domain.*
 
 
 object AuthHandlers:
@@ -17,15 +14,15 @@ object AuthHandlers:
     ZIO
       .fromTry(password.bcrypt())
       .mapError(e => PasswordHashingFailedError(e.getMessage))
-      
+
   private def generateToken(userId: UserId): ZIO[JwtService, AuthError, String] =
     ZIO.serviceWithZIO[JwtService](
       _.jwtEncode(userId)
     ).mapError(e => AuthError(e.getMessage))
-    
+
   def loginHandler(credentials: Credentials): ZIO[AuthEnv, Error, String] =
     for
-      passwordHash <- hashPassword(credentials.password) 
+      passwordHash <- hashPassword(credentials.password)
 
       maybeUid <- ZIO.serviceWithZIO[UserRepo](_.getUidByCredentials(
         credentials.username,
@@ -33,19 +30,18 @@ object AuthHandlers:
       )).mapError(e => AuthError(e.getMessage))
 
       token <- maybeUid match
-        case Some(uid: UserId) => generateToken(uid) 
+        case Some(uid: UserId) => generateToken(uid)
         case None => ZIO.fail(InvalidCredentialsError())
 
     yield token
-    
-  def signupHandler(credentials: Credentials): ZIO[AuthEnv, Error, String] = 
+
+  def signupHandler(credentials: Credentials): ZIO[AuthEnv, Error, String] =
     for
       passwordHash <- hashPassword(credentials.password)
-      
+
       uid <- ZIO.serviceWithZIO[UserRepo](_.add(
-        User(credentials.username, passwordHash) 
+        User(credentials.username, passwordHash)
       )).mapError(e => AuthError(e.getMessage))
-      
+
       token <- generateToken(uid)
-    yield token 
-    
+    yield token
