83f1e354e07bdb29b62391515df2aacb1eba5464
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-05 00:00:25 +0300

    feat: add new error

--- src/main/scala/app/domain/Error.scala
+++ src/main/scala/app/domain/Error.scala
@@ -2,7 +2,7 @@ package app.domain
 
 import io.circe.*
 
-sealed trait Error(message: String) derives Codec.AsObject
+sealed trait Error(message: String) extends Throwable derives Codec.AsObject
 
 case class MovieError(message: String) extends Error(message)
 case class NoMovieWithGivenIdError(message: String = "No movie with given id found") extends Error(message)
@@ -11,3 +11,4 @@ case class NoMovieWithGivenIdError(message: String = "No movie with given id fou
 case class AuthError(message: String) extends Error(message)
 case class PasswordHashingFailedError(message: String) extends Error(message)
 case class InvalidCredentialsError(message: String = "Invalid credentials") extends Error(message)
+case class NoUserWithGivenIdError(message: String = "No user with given id found") extends Error(message)
