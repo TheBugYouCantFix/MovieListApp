aa4780711c5eb3627dc1924b24258a05b5cc15f2
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-05 23:13:06 +0300

    feat: add handlers to auth endpoints

--- src/main/scala/app/backend/auth/AuthEndpoints.scala
+++ src/main/scala/app/backend/auth/AuthEndpoints.scala
@@ -3,7 +3,7 @@ package app.backend.auth
 import sttp.tapir.ztapir.*
 import sttp.tapir.json.circe.*
 import sttp.tapir.generic.auto.*
-import app.backend.auth.requestmodels.UpdatePassword
+import app.backend.auth.requestmodels.{UpdatePassword, UpdateUsername}
 import app.backend.data.repositories.UserRepo
 import app.domain.{Credentials, Error}
 import app.utils.given
@@ -12,22 +12,34 @@ object AuthEndpoints:
   val login = endpoint
     .post
     .in(jsonBody[Credentials])
+    .out(plainBody[String])
     .errorOut(jsonBody[Error])
+    .zServerLogic(AuthHandlers.loginHandler)
+
   val signup = endpoint
     .post
     .in(jsonBody[Credentials])
+    .out(plainBody[String])
     .errorOut(jsonBody[Error])
+    .zServerLogic(AuthHandlers.signupHandler)
+
   val updateUsername = endpoint
+    .put
+    .in(jsonBody[UpdateUsername])
+    .errorOut(jsonBody[Error])
+    .zServerLogic(AuthHandlers.updateUsernameHandler)
+
+  val updatePassword = endpoint
     .put
     .in(jsonBody[UpdatePassword])
     .errorOut(jsonBody[Error])
-  val updatePassword = endpoint
-    .put
-    .in(jsonBody[Error])
-    .errorOut(jsonBody[Error])
+    .zServerLogic(AuthHandlers.updatePasswordHandler)
+
   val deleteAccount = endpoint
     .delete
     .in(jsonBody[Credentials])
     .errorOut(jsonBody[Error])
+    .zServerLogic(AuthHandlers.deleteUserHandler)
 
-  val endpoints = List(login, signup, updateUsername, updatePassword, deleteAccount)
+  val authorizationEndpoints = List(login, signup)
+  val endpoints = List(updateUsername, updatePassword, deleteAccount)
