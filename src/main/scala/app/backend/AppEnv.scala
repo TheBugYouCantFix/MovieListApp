fececc914a42e5452966317f6b039a38bed60dc7
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-09 02:22:04 +0300

    feat: add string to app env

--- src/main/scala/app/backend/AppEnv.scala
+++ src/main/scala/app/backend/AppEnv.scala
@@ -3,4 +3,4 @@ package app.backend
 import data.repositories.*
 import auth.jwt.JwtService
 
-type AppEnv = MovieRepo & UserRepo & JwtService
+type AppEnv = MovieRepo & UserRepo & JwtService & String
