3b6e7684d1b216eb9b157e21fef8d3348287f5a6
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-06-14 04:18:32 +0300

    fix: change layer order

--- src/main/scala/app/backend/MovieListApp.scala
+++ src/main/scala/app/backend/MovieListApp.scala
@@ -3,8 +3,12 @@ package app.backend
 import zio.*
 import zio.http.*
 import sttp.tapir.*
-import sttp.tapir.server.ziohttp.ZioHttpInterpreter
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
+import sttp.tapir.server.interceptor.log.{DefaultServerLog, ServerLog}
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
+
+import sttp.tapir.server.interceptor.log.{DefaultServerLog, ServerLog}
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
 import movie.MovieEndpoints
 import app.backend.auth.AuthEndpoints
 import app.backend.auth.jwt.{JwtConfig, JwtService}
@@ -12,30 +16,29 @@ import data.dbLayer
 import data.repositories.{MovieRepo, MovieRepoLive, UserRepo}
 
 object MovieListApp extends ZIOAppDefault:
-  val swaggerEndpoints = ZioHttpInterpreter().toHttp(
-    SwaggerInterpreter()
+  val swaggerEndpoints = ZioHttpInterpreter().toHttp( SwaggerInterpreter()
       .fromServerEndpoints(
         MovieEndpoints.endpoints ++ AuthEndpoints.allEndpoints,
         "MovieListApp",
         "1.0"
       )
   )
+
   val endpoints: Routes[AppEnv, Response] =
       ZioHttpInterpreter().toHttp(
-        MovieEndpoints.endpoints ++ AuthEndpoints.allEndpoints
-    )
+        MovieEndpoints.endpoints ++ AuthEndpoints.allEndpoints 
+    ) ++ swaggerEndpoints
 
-  val app = (swaggerEndpoints ++ endpoints) @@ Middleware.debug
+  val app = endpoints @@ Middleware.debug
 
   override def run =
     Server.serve(app)
       .provide(
         ZLayer.succeed(Server.Config.default.port(8080)),
+        dbLayer,
+        JwtConfig.defaultLayer,
         MovieRepo.layer,
         UserRepo.layer,
-        JwtConfig.defaultLayer,
         JwtService.layer,
-        dbLayer,
         Server.live
       )
-      .exitCode
